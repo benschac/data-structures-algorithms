c2ae964d3d12c5ac24f8abc814f9e205d9371c4e
Bejamin Schachter <benschac@gmail.com>
2017-02-28 23:13:34 -0500

    removes comments

--- stack/stack.js
+++ stack/stack.js
@@ -1,10 +1,3 @@
-// To-Do, ditch array and create the underlying structure.
-
-// Using the new Symbol primitive is an option, but you
-// can still mutate the array in different places if you wish.
-// ex.
-// let _items = Symbol();
-// let items = new WeakMap();
 
 class Node {
   constructor(data) {
@@ -56,49 +49,20 @@ class Stack {
 
   constructor() {
     this.list = new StackImplementation();
-    // Creates public instance varible that can be mutated.
-    // this.list = [];
-
-    // Symbol Implementation
-    // this[_items] = [];
   }
 
   push(element) {
     this.list.push(element)
-
-
-    // s.push(element);
-
-    // Symbol Implementation
-    // this[_items].push();
-
-    // Instance varible Implementation.
-    // this.list.push(element);
   }
 
   pop() {
 
     this.list.pop();
-    // let s = items.get(this);
-    // s.pop(element);
 
-    // Symbol Implementation
-    // this[_items].pop()
-
-    // Instance varible Implementation.
-    // this.list.pop();
   }
 
   peek() {
-
-    // let s = items.get(this);
-    // return s[s.length - 1];
-
-    // Instance varible Implementation
-    // return this.list[this.list.length - 1];
-
-    // Symbol Implementation
-    // return this[_items][this[_items].length - 1];
+    this.list.peek();
   }
 
   print() {
@@ -106,100 +70,15 @@ class Stack {
   }
 
   size() {
-
-    console.log(this.list.count());
-
-    // let s = items.get(this);
-    // return s.length;
-
-    // Symbol Implementation
-    // this[_items].length;
-
-    // Instance varible implementation
-    // return this.list.length;
+    this.list.count();
   }
 
   isEmpty() {
-
-    let s = items.get(this);
-    return s.length === 0;
-
-    // Instance method implementation.
-    // return this.list.length === 0;
+    this.list.isEmpty();
   }
 
   clear() {
-    let s = items.get(this);
-    s.push(element);
-
-    // Instance varible method
-    // this.list = [];
+    this.list = new StackImplementation();
   }
 
 }
-
-let stack = new StackImplementation();
-stack.push('helo');
-stack.push('hello again');
-stack.push('to remove');
-stack.pop();
-stack.print();
-stack.peek();
-
-
-
-// let stack = new Stack();
-// stack.push('helo');
-// stack.push('hello again');
-// stack.push('to remove');
-// stack.pop();
-// stack.print();
-
-// console.log(stack.push('push again'));
-
-
-// MUTATION OF ARRAY USING SYMBOL PRIMITIVE
-// let stack = new Stack();
-// stack.push(8);
-// let objectSymbols = Object.getOwnPropertySymbols(stack);
-// console.log(objectSymbols.length); // 1
-// console.log(objectSymbols); // [Symbol()]
-// console.log(objectSymbols[0]); // Symbol()
-// stack[objectSymbols[0]].push(1);
-// stack.print();
-
-
-
-// Using a constructor function implementation.
-// function StackConstructor() {
-//   let items = [];
-//
-//   // Push element on to the stack.
-//   this.push = element => {
-//     items.push(element);
-//   }
-//
-//   // remove element from the top of the stack.
-//   this.pop = () => {
-//     items.pop();
-//   }
-//
-//   // Look at the last element of the stack.
-//   this.peek = () => {
-//     return items[items.length - 1];
-//   }
-//
-//   // Get the size of the stack.
-//   this.size = () => {
-//     return items.length;
-//   }
-//
-//   this.isEmpty = () => {
-//     return arr.length === 0;
-//   }
-//
-//   this.clear = () => {
-//     items = [];
-//   }
-// }
-//
