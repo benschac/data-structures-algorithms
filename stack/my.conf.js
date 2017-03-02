c47dfd87c6b938dcaf98cdc2de73f1d78d9a7071
Bejamin Schachter <benschac@gmail.com>
2017-03-02 09:59:13 -0500

    adds mocha and chai

--- stack/my.conf.js
+++ stack/my.conf.js
@@ -10,13 +10,14 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'requirejs'],
+    frameworks: ['jasmine', 'requirejs', 'mocha', 'chai'],
 
 
     // list of files / patterns to load in the browser
     files: [
       'test-main.js',
-      {pattern: 'test/*Spec.js', included: false}
+      {pattern: 'test/*Spec.js', included: false},
+      {pattern: 'stack/*.js', included: false}
     ],
 
 
---
+++ stack/test/stack.testSpec.js
@@ -0,0 +1,5 @@
+describe('Stack', () => {
+  it('should load on the screen');
+  it('be super fun and cool and stuff');
+  it('another it block');
+});
